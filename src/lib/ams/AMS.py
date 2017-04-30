9fd83f8f810a9b50c8f55821ddab6447fd92ef4c
Stefan Nožinić <stefan@lugons.org>
2017-04-30 17:19:04 +0200

    Modify AMS to do not depend on specific database organization

--- src/lib/ams/AMS.py
+++ src/lib/ams/AMS.py
@@ -14,50 +14,23 @@ class AMS(object):
         AMSDirectorySizeAction
     ]
 
-    def __init__(self, dbpath):
-        self.dbpath = dbpath
-
-    def measure(self, project, subproject):
+    def measure(self, project, subproject, ms):
         """
         Returns list of measurements or throws ManualInputRequiredException
+
+        ms: list of measuring dictionary with the following fields
+            
+            action: action (method) to perform measurement
+            ... every other field is specific to the action 
         """
         res = [] 
-        spdir = "%s/%s/%s" % (self.dbpath, project.getName(), subproject.getName())
-        if os.path.exists("%s/measure.json" % spdir):
-            f = open("%s/measure.json" % spdir, "r")
-            cfg = json.loads(f.read())
-            f.close()
-            ms = cfg["measurings"]
-            for measurement in ms:
-                for action in self.ACTIONS:
-                    if measurement["action"] == action.name:
-                        action_obj = action(measurement)
-                        res.append(action_obj.measure())
-                        break
-            return res
+        for measurement in ms:
+            for action in self.ACTIONS:
+                if measurement["action"] == action.name:
+                    action_obj = action(measurement)
+                    res.append(action_obj.measure())
+                    break
+        return res
 
-
-    def append(self, project, subproject, measurements):
-        """
-        Adds measurement results into database for specified project and subproject
-        """
-        today = datetime.date.today().strftime("%d.%m.%Y.")
-        res = [today]
-        for m in measurements:
-            res.append(str(m))
-        nline = ",".join(res)
-        lines = []
-        fname = "%s/%s/%s/progress.csv" % (self.dbpath, project.getName(), subproject.getName())
-        f = open(fname, "r")
-        for line in f:
-            lines.append(line)
-        f.close()
-        if today == lines[-1].split(",")[0]: 
-            return 
-        lines.append(nline)
-        f = open(fname, "w")
-        for line in lines:
-            f.write(line)
-        f.close()
 
 
--- src/test/TestAMS.py
+++ src/test/TestAMS.py
@@ -3,12 +3,16 @@ import unittest
 from ..lib.ams import * 
 from ..lib.schedule import * 
 
+import json 
+
 class TestAMS(unittest.TestCase):
     
     def test_appending(self):
-        ams = AMS("test_data/db/")
+        fname = "test_data/db/Notes/Size/measure.json"
         project = ScheduleProject("Notes", (0,0,0), (0,0,0,))
         subproject = ScheduleSubproject("Size")
-        m = ams.measure(project, subproject)
+        ams = AMS()
+        f = open(fname)
+        m = ams.measure(project, subproject, json.loads(f.read())["measurings"])
+        f.close()
         self.assertEqual(len(m), 3)
-        ams.append(project, subproject, m)
