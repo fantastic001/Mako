025cbc8a84ea31430d279e19404c73736d26fc87
Stefan Nožinić <stefan@lugons.org>
2017-05-21 16:48:13 +0200

    Implement method for creating action objects based on measurement dict

--- src/lib/ams/AMS.py
+++ src/lib/ams/AMS.py
@@ -21,16 +21,25 @@ class AMS(object):
         ms: list of measuring dictionary with the following fields
             
             action: action (method) to perform measurement
+            id: id of specific action in database 
+            description: description of concrete action
             ... every other field is specific to the action 
         """
         res = [] 
         for measurement in ms:
-            for action in self.ACTIONS:
-                if measurement["action"] == action.name:
-                    action_obj = action(measurement["id"], measurement.get("description", ""), measurement)
-                    res.append(action_obj.measure())
-                    break
+                res.append(self.getAction(measurement).measure())
         return res
+
+    def getAction(self, measurement):
+        """
+        Returns concrete action specified by ms dictionary
+
+        see documentation for measure(...) for morre info about data in dictioary 
+        """
+        for action in self.ACTIONS:
+            if measurement["action"] == action.name:
+                return action(measurement["id"], measurement.get("description", ""), measurement)
+
 
 
 
