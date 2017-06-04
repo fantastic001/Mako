345b006b387bf8ace06a0324c1b80afcc4e6a0e8
Stefan Nožinić <stefan@lugons.org>
2017-06-04 17:47:16 +0200

    Make CLI wrapper and setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'mako',
+  packages = ['mako', 'mako.lib', "mako.lib.schedule", "mako.lib.schedule.formats", "mako.lib.ams", "mako.lib.ams.actions", "mako.lib.reporting", "mako.lib.database", "mako.desktop"], # this must be the same as the name above
+  version = '0.1',
+  description = 'Mako is a tool for organizing your projects, schedules, events and to help you live more agile life.',
+  author = 'Stefan Nožinić',
+  author_email = 'stefan@lugons.org',
+  url = 'https://github.com/fantastic001/Mako', # use the URL to the github repo
+  download_url = 'https://github.com/fantastic001/Mako/tarball/0.1',
+  keywords = ['agile', 'schedules', 'projects', 'organization'], 
+  package_dir = {'mako': 'src/'},
+  classifiers = [],
+  scripts = ["bin/mako"],
+  install_requires=["ArgumentStack", "YAPyOrg"] # dependencies listed here 
+)
