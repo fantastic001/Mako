b8d9cffae6889c510c7425f707fee88b8e9f539e
Stefan Nožinić <stefan@lugons.org>
2017-06-06 16:41:06 +0200

    New version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'mako',
   packages = ['mako', 'mako.lib', "mako.lib.schedule", "mako.lib.schedule.formats", "mako.lib.ams", "mako.lib.ams.actions", "mako.lib.reporting", "mako.lib.reporting.month", "mako.lib.reporting.quarter", "mako.lib.database", "mako.desktop"], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'Mako is a tool for organizing your projects, schedules, events and to help you live more agile life.',
   author = 'Stefan Nožinić',
   author_email = 'stefan@lugons.org',
   url = 'https://github.com/fantastic001/Mako', # use the URL to the github repo
-  download_url = 'https://github.com/fantastic001/Mako/tarball/0.1',
+  download_url = 'https://github.com/fantastic001/Mako/tarball/0.2',
   keywords = ['agile', 'schedules', 'projects', 'organization'], 
   package_dir = {'mako': 'src/'},
   classifiers = [],
