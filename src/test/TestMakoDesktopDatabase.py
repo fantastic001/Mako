bdcb377293ffe2e6510a87cd12e7067a96789495
Stefan Nožinić <stefan@lugons.org>
2017-07-11 23:42:12 +0200

    Added additional tests for desktop database

--- src/test/TestMakoDesktopDatabase.py
+++ src/test/TestMakoDesktopDatabase.py
@@ -51,3 +51,23 @@ class TestMakoDesktopDatabase(unittest.TestCase):
 
     def test_project_upload(self):
         self.DUD(self.db.downloadProjects, self.db2.uploadProjects, self.db2.downloadProjects)
+    
+    def test_measurement_actions_upload(self):
+        self.DUD(self.db.downloadMeasurementActions, self.db2.uploadMeasurementActions, self.db2.downloadMeasurementActions)
+
+    def test_measurement_data_upload(self):
+        data = self.db.downloadMeasurementData("measureNotes")
+        self.db2.uploadMeasurementData("measureNotes", data)
+        data2 = self.db2.downloadMeasurementData("measureNotes")
+        self.assertEqual(len(data), len(data2)+1)
+    def test_upload_reports(self):
+        self.DUD(self.db.downloadReports, self.db2.uploadReports, self.db2.downloadReports)
+
+    def test_schedule_conditions_upload(self):
+        self.DUD(self.db.downloadDefaultConditions, self.db2.uploadDefaultConditions, self.db2.downloadDefaultConditions)
+
+    def test_upload_schedules(self):
+        self.DUD(self.db.downloadSchedules, self.db2.uploadSchedules, self.db2.downloadSchedules)
+        
+    def test_upload_tables(self):
+        self.DUD(self.db.downloadTables, self.db2.uploadTables, self.db2.downloadTables)
