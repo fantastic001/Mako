184ed5de90737d1490d249feda53c366f825ec4d
Stefan Nožinić <stefan@lugons.org>
2017-07-11 15:55:55 +0200

    Add unit tests for task warrior db

--- src/test/TestMakoTaskWarriorDatabase.py
+++ src/test/TestMakoTaskWarriorDatabase.py
@@ -19,3 +19,18 @@ class TestMaoTaskWarriorDatabase(unittest.TestCase):
         db2.uploadProjects(projects)
         projects2 = db2.downloadProjects()
         self.assertEqual(len(projects), len(projects2))
+
+    def test_project_names(self):
+        projects = self.db.downloadProjects() 
+        names = [] 
+        for proj in projects:
+            names.append(proj.getName())
+        self.assertEqual(sorted(names), sorted(["unknown", "life"]))
+    
+    def test_tasks_download(self):
+        projects = self.db.downloadProjects()
+        t = [] 
+        for p in projects:
+            for s in p.getSubprojects():
+                t = t + s.getAllTasks()
+        self.assertEqual(len(t), 3)
