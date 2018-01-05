1fe426740fc2ddb40642d99caa4802ffea62a5e7
Stefan Nožinić <stefan@lugons.org>
2018-01-05 15:52:15 +0100

    Do not use memory database in makoc and implement adding subproject

--- src/lib/database/MakoDatabase.py
+++ src/lib/database/MakoDatabase.py
@@ -185,6 +185,7 @@ class MakoDatabase(object):
             db.uploadMeasurementData(action.getIdentifier(), self.downloadMeasurementData(action.getIdentifier()))
         db.uploadData(self.downloadData())
         db.uploadSchedules(self.downloadSchedules())
+        print(self.downloadTables())
         db.uploadTables(self.downloadTables())
         db.uploadReports(self.downloadReports())
         db.uploadDefaultConditions(self.downloadDefaultConditions())
